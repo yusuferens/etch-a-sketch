a904c2d262e6427bca85f7eaa4b9111ce6253d7a
yusuferens <yusuferenketen@gmail.com>
2023-12-07 23:11:51 +0300

    implemented functions for buttons

--- script.js
+++ script.js
@@ -1,25 +1,60 @@
- let board = document.querySelector(".board");
+document.getElementById('popup').addEventListener('click', askForSize);
+document.querySelector('.btn-dark').addEventListener('click', () => setColor('black'));
+document.querySelector('.btn-warning').addEventListener('click', () => setColor('random'));
+document.querySelector('.btn-info').addEventListener('click', resetGrid);
+
+ 
+ function setColor(cell){
+    let color 
+
+    if ( cell === 'black' || cell === 'random'){
+        color = cell
+    } else {
+        color = 'black'
+    }
+
+    if (color === 'random') {
+        cell.style.backgroundColor = getRandomColor()
+    } else {
+        cell.style.backgroundColor = color
+    }
+ }
+
+ 
+ 
+ function resetGrid() {
+    cell.style.backgroundColor = 'white'
+
+ }
  
 
  function askForSize(){
     //ask for the size of the game board
-    let size = prompt("Please enter the size(The size should be below 64x64):")
+    let size = prompt("Please enter the size (default is 16):")
 
     if(size !== null && size ==! ""){
-        processSize(size)
+        createGrid(size)
     
     }else if(size > 64){
         alert("The number should be below  or equal 64!")
     }else{
-        processSize(16)
+        createGrid(16)
     }    
 }
  
 
- function processSize(size){
-    board.innerHTML = ""
+ function createGrid(size){ //process the entered size and create the game board
+    board.innerHTML = ""    
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
-    //process the entered size and create the game board
+    for(let i = 0; i < size * size; i++){
+        let cell = document.createElement('div');
+        cell.classList.add("cell");
+        board.appendChild(cell)
 
+        cell.addEventListener("mouseover",function(){
+            setColor(cell)
+        })
+    }
  }
 
