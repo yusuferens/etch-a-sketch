45def9433962fbe45863c5a41c84c4ce43a5998f
yusuferens <yusuferenketen@gmail.com>
2023-12-07 23:27:43 +0300

    fixed button selectors, event listeners, resetGrid

--- script.js
+++ script.js
@@ -1,60 +1,62 @@
-document.getElementById('popup').addEventListener('click', askForSize);
-document.querySelector('.btn-dark').addEventListener('click', () => setColor('black'));
-document.querySelector('.btn-warning').addEventListener('click', () => setColor('random'));
-document.querySelector('.btn-info').addEventListener('click', resetGrid);
+let board = document.querySelector(".board");
+let blackButton = document.querySelector(".btn-black");
+let randomButton = document.querySelector(".btn-warning");
+let resetButton = document.querySelector(".btn-info");
 
- 
- function setColor(cell){
-    let color 
+blackButton.addEventListener('click', () => setColor('black'));
+randomButton.addEventListener('click', () => setColor('random'));
+resetButton.addEventListener('click', resetGrid);
 
-    if ( cell === 'black' || cell === 'random'){
-        color = cell
+function askForSize() {
+    let size = prompt("Please enter the size (default is 16):");
+
+    if (size !== null && size !== "") {
+        createGrid(size);
+    } else if (size > 64) {
+        alert("The number should be below or equal to 64!");
     } else {
-        color = 'black'
+        createGrid(16);
+    }
+}
+
+function createGrid(size) {
+    board.innerHTML = "";
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add("cell");
+        board.appendChild(cell);
+
+        cell.addEventListener("mouseover", function () {
+            setColor(cell);
+        });
+    }
+}
+
+function setColor(cell) {
+    let color;
+
+    if (cell === 'black' || cell === 'random') {
+        color = cell;
+    } else {
+        color = 'black';
     }
 
     if (color === 'random') {
-        cell.style.backgroundColor = getRandomColor()
+        cell.style.backgroundColor = getRandomColor();
     } else {
-        cell.style.backgroundColor = color
+        cell.style.backgroundColor = color;
     }
- }
-
- 
- 
- function resetGrid() {
-    cell.style.backgroundColor = 'white'
-
- }
- 
-
- function askForSize(){
-    //ask for the size of the game board
-    let size = prompt("Please enter the size (default is 16):")
-
-    if(size !== null && size ==! ""){
-        createGrid(size)
-    
-    }else if(size > 64){
-        alert("The number should be below  or equal 64!")
-    }else{
-        createGrid(16)
-    }    
 }
- 
 
- function createGrid(size){ //process the entered size and create the game board
-    board.innerHTML = ""    
-    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+function resetGrid() {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.style.backgroundColor = 'white';
+    });
+}
 
-    for(let i = 0; i < size * size; i++){
-        let cell = document.createElement('div');
-        cell.classList.add("cell");
-        board.appendChild(cell)
-
-        cell.addEventListener("mouseover",function(){
-            setColor(cell)
-        })
-    }
- }
-
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
